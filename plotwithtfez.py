7dac1e70d643bd0982322e154806da51d39fee65
PrManwani <pkmanwani@gmail.com>
2019-01-14 16:31:38 -0800

    Update plotwithtfez.py

--- plotwithtfez.py
+++ plotwithtfez.py
@@ -2,7 +2,19 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-main = '/home/pratik/hoffman/FEZ-XZ'
+from pylab import *
+
+params = {
+   'axes.labelsize': 8,
+   'legend.fontsize': 10,
+   'xtick.labelsize': 10,
+   'ytick.labelsize': 10,
+   'text.usetex': False,
+   'figure.figsize': [6, 4.5]
+   }
+rcParams.update(params)
+
+main = '/home/pratik/Desktop/Resonantcase/10_2.4E6/FEZ-XZ'
 pathname = []
 for file in os.listdir(main):
 	if file.endswith(".h5"):
@@ -16,7 +28,7 @@ for j, name in enumerate(pathname):
 	fez = []
 	fez = h5py.File(pathname[j], 'r')
 	fez = fez[u'FEZ-XZ']
-	fez = fez[:,128]*1.6
+	fez = fez[:,128]*1.6 #mcw_p is 1.6TeV here
 	print(fez.min())
 	#FEZ = np.asarray(FEZ)
 	allfez.append(fez)
@@ -25,14 +37,22 @@ for j, name in enumerate(pathname):
 	#print(pathname[j])
 	#FEZ[j] = FEZ[j][u'FEZ-XZ']
 allfez = np.asarray(allfez)
-mult = float(float(512)/float(30))
-beam_pos = [round(mult*4),round(mult*6),round(mult*8),round(mult*10),round(mult*12),round(mult*14),round(mult*16),round(mult*18),round(mult*20),round(mult*22),round(mult*23.55) ]
+mult = float(float(512)/float(30)) #30 is the box width and 512 is the number of grid points(2^9)
+beam_pos = [4,6,8,10,12,14,16,18,20,22]
+#big_beam_pos = [22]
+witness_pos = [23.55]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+
+
 print(np.shape(allfez))
 
 number = np.shape(allfez)[0]
 print(number)
 number = number
 f, axarr = plt.subplots(number, sharex=True, sharey=True)
+
 
 '''#f.suptitle('Sharing both axes')
 for i in range(number):
@@ -45,13 +65,28 @@ f.subplots_adjust(hspace=0)
 for ax in axarr:
     ax.label_outer()
 #plt.show()'''
-f,axarr = plt.subplots(1, sharex=True, sharey=True)
-plt.ylabel('TeV/m')
-plt.title('9 bunches of 2.4E6 followed by 24E6 bunch and a witness bunch')
-axarr.plot(allfez[0])
-axarr.plot(beam_pos, [0,0,0,0,0,0,0,0,0,0,0], 'ro')
+
+
+#f,axarr = plt.subplots(1, sharex=True, sharey=True)
+
+axes(frameon=0)
+plt.ylabel(r'FEZ (TeV/m)')
+plt.xlabel(r'$\zeta$ $(\mu m)$')
+#plt.title('10 bunches followed by a witness, Q~ = 2.67')
+#plt.plot(number,q,marker = 'o',linewidth=2,color='#006BB2')
+#lt.grid(True)
+#axarr.plot(allfez[0])
+
+plt.plot(np.linspace(0,30,512),allfez[0],linewidth=2,color='#006BB2')
 #axarr.plot(big_beam_pos, [0], 'ro', markersize= 15)
-#axarr.plot(witness_pos, [0], 'rp')
+plt.plot(beam_pos, [0,0,0,0,0,0,0,0,0,0], 'ro')
+#axarr.plot(big_beam_pos, [0], 'ro', markersize = '12')
+
+
+plt.plot(witness_pos, [0], 'rp')
+
+xticks(np.arange(0, 32, 2))
+yticks(np.arange(-1,1,0.2))
 '''axarr[1].plot(allfez[-1])
 axarr[1].plot(beam_pos, [0,0,0,0,0,0,0,0,0,0,0], 'ro')
 f.subplots_adjust(hspace=0)
