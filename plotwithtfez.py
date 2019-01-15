9ae943c0063d57275dc8cc730ea171334a12c2fa
PrManwani <pkmanwani@gmail.com>
2019-01-14 16:47:04 -0800

    Update plotwithtfez.py

--- plotwithtfez.py
+++ plotwithtfez.py
@@ -72,7 +72,7 @@ for ax in axarr:
 axes(frameon=0)
 plt.ylabel(r'FEZ (TeV/m)')
 plt.xlabel(r'$\zeta$ $(\mu m)$')
-#plt.title('10 bunches followed by a witness, Q~ = 2.67')
+plt.title('10 bunches followed by a witness, Q~ = 2.67') #Q~ = (m*N_b*k_p^3)/(n_o)
 #plt.plot(number,q,marker = 'o',linewidth=2,color='#006BB2')
 #lt.grid(True)
 #axarr.plot(allfez[0])
